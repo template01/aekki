416e00264176cff97c87b57fd8e43ac8a301223f
lasbian <lasbian@debian.home>
2018-07-04 11:18:52 +0200

    env test

--- aekki-front/store/index.js
+++ aekki-front/store/index.js
@@ -14,10 +14,10 @@ export const mutations = {
 
   SET_GLOBALROOTAPI(state, url) {
     state.test = url
-    if(url === 'http://localhost:3002'){
+    if(url === 'localhost:3002'){
       state.rootApi = 'http://localhost:1337'
     }
-    if(url === 'http://aekki.parkedthis.space'){
+    if(url === 'aekki.parkedthis.space'){
       state.rootApi = 'http://aekki-api.parkedthis.space'
     }
   },
@@ -30,9 +30,10 @@ export const actions = {
   // ONLY WORKS IN INDEX JS - THIS WILL SET/COMMIT THE COUPLED MODULES
 
   async nuxtServerInit({
-    commit,store,dispatch,state,context
-  }) {
-    commit('SET_GLOBALROOTAPI',process.env.baseUrl)
+    commit,store,dispatch,state
+  },{req}) {
+    commit('SET_GLOBALROOTAPI',req.headers.host)
+    // console.log(req.headers.host)
 
     const { data } = await axios.get(state.rootApi+'/productview')
     commit('viewing/SET_ROOTAPI',state.rootApi)
